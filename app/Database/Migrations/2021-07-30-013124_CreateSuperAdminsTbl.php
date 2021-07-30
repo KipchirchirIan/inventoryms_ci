c455e85b1b8bd63c46a654cebf6498271708a5cb
Ian Kipchirchir <potterke4@gmail.com>
2021-07-30 08:34:23 +0300

    Add database migrations

---
+++ app/Database/Migrations/2021-07-30-013124_CreateSuperAdminsTbl.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateSuperAdminsTbl extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+		    'sadmin_id' => [
+		        'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+		    'first_name' => [
+		        'type' => 'VARCHAR',
+                'constraint' => 50
+            ],
+            'last_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+                'unique' => true,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'created_at timestamp default current_timestamp',
+            'updated_at timestamp default current_timestamp on update current_timestamp',
+        ]);
+		$this->forge->addPrimaryKey('sadmin_id');
+		$this->forge->createTable('tbl_superadmins');
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('tbl_superadmins');
+	}
+}
---
+++ app/Database/Migrations/2021-07-30-020339_CreateAdminsTbl.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateAdminsTbl extends Migration
+{
+	public function up()
+	{
+		$this->forge->addField([
+		    'admin_id' => [
+		        'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'first_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'last_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+            ],
+            'position' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50,
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+                'unique' => true,
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'created_at timestamp default current_timestamp',
+            'updated_at timestamp default current_timestamp on update current_timestamp',
+        ]);
+		$this->forge->addPrimaryKey('admin_id');
+		$this->forge->createTable('tbl_admins');
+	}
+
+	public function down()
+	{
+		$this->forge->dropTable('tbl_admins');
+	}
+}
