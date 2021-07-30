b7ec8c07961eaf3d2737923797cd1a64be9d7044
Ian Kipchirchir <potterke4@gmail.com>
2021-07-30 09:03:54 +0300

    Seed super admins table

---
+++ app/Database/Seeds/SuperAdminSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class SuperAdminSeeder extends Seeder
+{
+	public function run()
+	{
+		$data = [
+		    'first_name' => 'Ian',
+            'last_name' => 'Kipchirchir',
+            'email' => 'potterke4@gmail.com',
+            'password' => password_hash('1234', PASSWORD_DEFAULT),
+        ];
+
+		$this->db->table('tbl_superadmins')->insert($data);
+	}
+}
