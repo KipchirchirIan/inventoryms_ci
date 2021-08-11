f44dfb6395ea7f85d562a2b98f868fbe49198473
Ian Kipchirchir <potterke4@gmail.com>
2021-08-11 13:36:37 +0300

    Add default/starter controller and view

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -2,17 +2,10 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class Dashboard extends BaseController
 {
     public function index()
 	{
-	    helper('html');
-
-	    $data = [
-	        'page_title' => 'Dashboard',
-            'first_uri_segment' => $this->request->uri->getSegment(1),
-        ];
-
-		return view('dash', $data);
+	    return redirect()->to('employee/dashboard');
 	}
 }
