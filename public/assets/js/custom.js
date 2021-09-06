1e7b58f778b029b9568447db410c8c9edec6a4f7
Ian Kipchirchir <potterke4@gmail.com>
2021-09-06 10:35:51 +0300

    Disable submit button on form submit

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -2,9 +2,23 @@
  *
  * You can write your JS code here, DO NOT touch the default style file
  * because it will make it harder for you to update.
- * 
+ *
  */
 
 "use strict";
 
-$("#itemsTable").dataTable();
+$(document).ready(function () {
+    $("#itemsTable").dataTable();
+
+    /* Disable submit buttons after form submit
+       to avoid double submissions
+     */
+    $('form').on('submit', function (e) {
+        // e.preventDefault();
+        // alert('Form submitted');
+
+        $(':submit').attr('disabled', true)
+            .attr('aria-disabled', 'true')
+            .addClass('disabled');
+    });
+})
