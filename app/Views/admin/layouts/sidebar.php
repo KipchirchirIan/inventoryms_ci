3af2c8047b9084b1221d40bde61274c507654641
Ian Kipchirchir <potterke4@gmail.com>
2022-01-19 20:18:24 +0300

    Show active link for current page

--- app/Views/admin/layouts/sidebar.php
+++ app/Views/admin/layouts/sidebar.php
@@ -8,12 +8,12 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class="dropdown active">
-                <a href="<?= base_url('admin/dashboard') ?>" class="nav-link"><i class="fas fa-columns"></i><span>Dashboard</span></a>
+            <li class="dropdown<?= service('uri')->getSegment(2) === 'dashboard' ? ' active' : '' ?>">
+                <a href="<?= base_url('admin/dashboard') ?>" class="nav-link<?= service('uri')->getSegment(2) === 'dashboard' ? ' active' : '' ?>"><i class="fas fa-columns"></i><span>Dashboard</span></a>
             </li>
             <li class="menu-header">Quick Actions</li>
-            <li class="dropdown">
-                <a href="<?= base_url('admin/item/checkInOut') ?>" class="nav-link"><i class="fas fa-sort"></i><span>Check In/Out</span></a>
+            <li class="dropdown<?= service('uri')->getSegment(3) === 'checkInOut' ? ' active' : '' ?>">
+                <a href="<?= base_url('admin/item/checkInOut') ?>" class="nav-link<?= service('uri')->getSegment(3) === 'checkInOut' ? ' active' : '' ?>"><i class="fas fa-sort"></i><span>Check In/Out</span></a>
             </li>
             <li class="menu-header">Manage Users</li>
             <li class="dropdown">
@@ -24,56 +24,56 @@
                     </li>
                 </ul>
             </li>
-            <li class="dropdown">
+            <li class="dropdown<?= service('uri')->getSegment(2) === 'employee' && (service('uri')->getSegment(3) === 'create' || service('uri')->getSegment(3) === 'index' || service('uri')->getSegment(3) === 'show') ? ' active' : '' ?>">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i
                             class="fas fa-user-friends"></i><span>Employees</span></a>
                 <ul class="dropdown-menu">
-                    <li class=""><a class="nav-link" href="<?php echo base_url('admin/employee/create') ?>">Add
+                    <li class="<?= service('uri')->getSegment(2) === 'employee' && service('uri')->getSegment(3) === 'create' ? 'active' : '' ?>"><a class="nav-link" href="<?php echo base_url('admin/employee/create') ?>">Add
                             Employee</a></li>
-                    <li class=""><a class="nav-link" href="<?php echo base_url('admin/employee/index') ?>">View
+                    <li class="<?= service('uri')->getSegment(2) === 'employee' && service('uri')->getSegment(3) === 'index' ? 'active' : '' ?>"><a class="nav-link" href="<?php echo base_url('admin/employee/index') ?>">View
                             Employees</a></li>
                 </ul>
             </li>
             <li class="menu-header">Manage Items</li>
-            <li class="dropdown">
+            <li class="dropdown<?= service('uri')->getSegment(2) === 'item' && (service('uri')->getSegment(3) === 'create' || service('uri')->getSegment(3) === 'index' || service('uri')->getSegment(3) === 'show') ? ' active' : '' ?>">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-list"></i>
                     <span>Items</span></a>
                 <ul class="dropdown-menu">
-                    <li class=""><a
+                    <li class="<?= service('uri')->getSegment(2) === 'item' && service('uri')->getSegment(3) === 'create' ? 'active' : '' ?>"><a
                                 class="nav-link"
                                 href="<?php echo base_url('admin/item/create'); ?>">Add Item</a>
                     </li>
-                    <li class=""><a
+                    <li class="<?= service('uri')->getSegment(2) === 'item' && service('uri')->getSegment(3) === 'index' ? 'active' : '' ?>"><a
                                 class="nav-link"
                                 href="<?php echo base_url('admin/item/index'); ?>">View Items</a>
                     </li>
                 </ul>
             </li>
             <li class="menu-header">Manage Item Categories</li>
-            <li class="dropdown">
+            <li class="dropdown<?= service('uri')->getSegment(2) === 'category' && (service('uri')->getSegment(3) === 'create' || service('uri')->getSegment(3) === 'index' || service('uri')->getSegment(3) === 'show') ? ' active' : '' ?>">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-th-large"></i>
                     <span>Item Categories</span></a>
                 <ul class="dropdown-menu">
-                    <li class=""><a
+                    <li class="<?= service('uri')->getSegment(2) === 'category' && service('uri')->getSegment(3) === 'create' ? 'active' : '' ?>"><a
                                 class="nav-link"
                                 href="<?php echo base_url('admin/category/create'); ?>">Add Category</a>
                     </li>
-                    <li class=""><a
+                    <li class="<?= service('uri')->getSegment(2) === 'category' && service('uri')->getSegment(3) === 'index' ? 'active' : '' ?>"><a
                                 class="nav-link"
                                 href="<?php echo base_url('admin/category/index'); ?>">View Categories</a>
                     </li>
                 </ul>
             </li>
             <li class="menu-header">Manage UoMs</li>
-            <li class="dropdown">
+            <li class="dropdown<?= service('uri')->getSegment(2) === 'uom' && (service('uri')->getSegment(3) === 'create' || service('uri')->getSegment(3) === 'index' || service('uri')->getSegment(3) === 'show') ? ' active' : '' ?>">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-balance-scale"></i>
                     <span>Unit of Measurements</span></a>
                 <ul class="dropdown-menu">
-                    <li class=""><a
+                    <li class="<?= service('uri')->getSegment(2) === 'uom' && service('uri')->getSegment(3) === 'create' ? 'active' : '' ?>"><a
                                 class="nav-link"
                                 href="<?php echo base_url('admin/uom/create'); ?>">Add UoM</a>
                     </li>
-                    <li class=""><a
+                    <li class="<?= service('uri')->getSegment(2) === 'uom' && service('uri')->getSegment(3) === 'index' ? 'active' : '' ?>"><a
                                 class="nav-link"
                                 href="<?php echo base_url('admin/uom/index'); ?>">View UoMs</a>
                     </li>
--- app/Views/layouts/sidebar.php
+++ app/Views/layouts/sidebar.php
@@ -8,27 +8,27 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class="dropdown active">
+            <li class="dropdown<?= service('uri')->getSegment(2) === 'dashboard' ? ' active' : '' ?>">
                 <a href="<?= base_url('employee/dashboard') ?>" class="nav-link"><i class="fas fa-columns"></i><span>Dashboard</span></a>
             </li>
             <li class="menu-header">Quick Actions</li>
-            <li class="dropdown">
+            <li class="dropdown<?= service('uri')->getSegment(3) === 'checkInOut' ? ' active' : '' ?>">
                 <a href="<?= base_url('employee/item/checkInOut') ?>" class="nav-link"><i class="fas fa-sort"></i><span>Check In/Out</span></a>
             </li>
             <li class="menu-header">Manage Users</li>
-            <li class="dropdown">
+            <li class="dropdown<?= service('uri')->getSegment(2) === 'employee' && (service('uri')->getSegment(3) === 'index' || service('uri')->getSegment(3) === 'show') ? ' active' : '' ?>">
                 <a href="#" class="nav-link has-dropdown" data-toggle="dropdown"><i class="fas fa-user-friends"></i><span>Employees</span></a>
                 <ul class="dropdown-menu">
-                    <li class=""><a class="nav-link" href="<?php echo base_url('employee/employee/index') ?>">View
+                    <li class="<?= service('uri')->getSegment(2) === 'employee' && service('uri')->getSegment(3) === 'index' ? 'active' : '' ?>"><a class="nav-link" href="<?php echo base_url('employee/employee/index') ?>">View
                             Employees</a></li>
                 </ul>
             </li>
             <li class="menu-header">Manage Items</li>
-            <li class="dropdown">
+            <li class="dropdown<?= service('uri')->getSegment(2) === 'item' && (service('uri')->getSegment(3) === 'create' || service('uri')->getSegment(3) === 'index' || service('uri')->getSegment(3) === 'show') ? ' active' : '' ?>">
                 <a href="#" class="nav-link has-dropdown"><i class="fas fa-list"></i>
                     <span>Items</span></a>
                 <ul class="dropdown-menu">
-                    <li class=""><a
+                    <li class="<?= service('uri')->getSegment(2) === 'item' && service('uri')->getSegment(3) === 'index' ? 'active' : '' ?>"><a
                                 class="nav-link"
                                 href="<?php echo base_url('employee/item/index'); ?>">View Items</a>
                     </li>
