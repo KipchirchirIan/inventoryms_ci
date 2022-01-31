f1d58bddc5adc2a4d25d3f13b336534312753bc1
Ian Kipchirchir <potterke4@gmail.com>
2022-01-31 13:30:53 +0300

    Fix: support button on sidebar to trigger emailing

--- app/Views/admin/layouts/sidebar.php
+++ app/Views/admin/layouts/sidebar.php
@@ -82,7 +82,7 @@
         </ul>
 
         <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
-            <a href="https://getstisla.com/docs" target="_blank"
+            <a href="mailto:support@bitrevlabs.co.ke" target="_blank"
                class="btn btn-primary btn-lg btn-block btn-icon-split">
                 <i class="fas fa-paper-plane"></i>Support
             </a>
